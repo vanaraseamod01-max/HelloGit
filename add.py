b54ae019f3501f4a417bf0ce5156d51b7657e42e
AmodLikesGreen <vanaraseamod01@gmail.com>
2025-11-26 23:59:08 +0530

    suceess second time

--- add.py
+++ add.py
@@ -5,3 +5,6 @@ if __name__ == "__main__":
    x= float(input("enter first number"))
    y= float(input("enter second number"))
    print(add(x,y))
+def add_three(a,b,c):
+   "return sum of three numbers"
+   return a+b+c
